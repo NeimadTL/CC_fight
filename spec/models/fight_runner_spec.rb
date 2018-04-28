b4cec3b1441c59e56c1d9de65a663f270bb15512
neimad971 <damientalbot26@gmail.com>
2018-04-28 21:04:48 +0200

    FightRunner class is tested by now

--- spec/models/fight_runner_spec.rb
+++ spec/models/fight_runner_spec.rb
@@ -1,24 +1,22 @@
-# require 'rails_helper'
-#
-# RSpec.describe FightRunner, type: :model do
-#
-#   Character.delete_all
-#   Fight.delete_all
-#
-#   strong_monster = Character.create!(name: "Strong monster", life_score: 50, attack_score: 95)
-#   weak_monster = Character.create!(name: "Weak monster", life_score: 35, attack_score: 60)
-#   fight = Fight.create!(first_character: strong_monster, second_character: weak_monster)
-#
-#   puts "#{fight.nil?}"
-#
-#
-#   describe "when there is a fight" do
-#
-#     it "the character with the strongest attack score should win" do
-#
-#       expect { FightRunner.new(fight).run_fight }.to
-#         match("#{strong_monster.name} won, life_score : #{strong_monster.life_score}")
-#     end
-#   end
-#
-# end
+require 'rails_helper'
+
+RSpec.describe FightRunner, type: :model do
+
+  Character.delete_all
+  Fight.delete_all
+
+  stronger_monster = Character.create!(name: "Strong monster", life_score: 50, attack_score: 95)
+  weaker_monster = Character.create!(name: "Weak monster", life_score: 35, attack_score: 60)
+  fight = Fight.create!(first_character: stronger_monster, second_character: weaker_monster)
+
+
+  describe "when there is a fight" do
+
+    it "the character with the strongest attack score should win" do
+      winner = FightRunner.new(fight).run_fight
+      expect(winner.id).to eq stronger_monster.id
+      expect(winner.name).to match("#{stronger_monster.name}")
+    end
+  end
+
+end
