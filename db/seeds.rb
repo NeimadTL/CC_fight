f498a1ab92371bad4716295e1295126c62916df7
neimad971 <damientalbot26@gmail.com>
2018-04-29 23:35:57 +0200

    images for seed characters have been

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,17 @@
 Character.delete_all
 Fight.delete_all
 
+def seed_image(file_name)
+  File.open(File.join(Rails.root, "/app/assets/images/#{file_name}"))
+end
 
-Character.create!(name: 'Game b0$$', life_score: 100, attack_score: 95)
-Character.create!(name: 'STr0ng mOn$tA', life_score: 100, attack_score: 70)
-Character.create!(name: 'C@pt@in c0ntr@t', life_score: 100, attack_score: 55)
-Character.create!(name: 'N0N@me', life_score: 100, attack_score: 30)
-Character.create!(name: 'CallMeLooser||KillMe', life_score: 100, attack_score: 10)
+Character.create!(name: 'Game b0$$', life_score: 100, attack_score: 95,
+  picture: seed_image('game_boss.jpg'))
+Character.create!(name: 'STr0ng mOn$tA', life_score: 100, attack_score: 70,
+  picture: seed_image('strong_monsta.jpg'))
+Character.create!(name: 'C@pt@in c0ntr@t', life_score: 100, attack_score: 55,
+  picture: seed_image('captain_contrat.jpeg'))
+Character.create!(name: 'Müm/\/\Y', life_score: 100, attack_score: 30,
+  picture: seed_image('mummy.jpg'))
+Character.create!(name: 'CallMeLooser||KillMe', life_score: 100, attack_score: 10,
+  picture: seed_image('call_me_looser_or_kill_me.jpg'))
