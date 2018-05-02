cb00a5aef73b0b25dd482e680c2b319c4da23b17
neimad971 <damientalbot26@gmail.com>
2018-05-03 01:57:05 +0200

    weapons are taken into account when there is a fight + fight fixtures update

--- app/models/fight_runner.rb
+++ app/models/fight_runner.rb
@@ -8,10 +8,15 @@ class FightRunner
   # Makes the characters go at each other and returns the winner
   # The winner is the character with the most attack score
   def run_fight
+    first_character_attack_score = @fight.first_character.attack_score
+      + @fight.first_character_weapon.attack_score
+    second_character_attack_score = @fight.second_character.attack_score
+      + @fight.second_character_weapon.attack_score
+
     while @fight.first_character.life_score > 0 && @fight.second_character.life_score > 0
-      if @fight.first_character.attack_score > @fight.second_character.attack_score
+      if first_character_attack_score > second_character_attack_score
         @fight.second_character.life_score = @fight.second_character.life_score - 10
-      elsif @fight.second_character.attack_score > @fight.first_character.attack_score
+      elsif second_character_attack_score > first_character_attack_score
         @fight.first_character.life_score = @fight.first_character.life_score - 10
       end
     end
