e0a1f2d4ccce05812bf247164389d9474f491390
neimad971 <damientalbot26@gmail.com>
2018-05-02 22:11:23 +0200

    weapons controller is tested

--- spec/controllers/weapons_controller_spec.rb
+++ spec/controllers/weapons_controller_spec.rb
@@ -2,4 +2,28 @@ require 'rails_helper'
 
 RSpec.describe WeaponsController, type: :controller do
 
+  describe "when GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "when POST #create" do
+    it "with good params, returns http redirect" do
+      post :create, weapon: { name: 'gun' }
+      created_weapon = Weapon.last
+      expect(created_weapon.name).to eq 'gun'
+      expect(flash[:notice]).to match('Weapon created with success')
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to(root_path)
+    end
+
+    it "with bad params, returns http unprocessable_entity" do
+      post :create, weapon: { name: nil }
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response).to render_template(:new)
+    end
+  end
+
 end
